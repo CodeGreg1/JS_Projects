1aa40f22d0648d8b3c29b112980bd78225989c47
GregWebDev <code@greggoodrem.co.uk>
2022-12-30 12:46:54 +0000

    Project 31 length correction

--- projects/31/script.js
+++ projects/31/script.js
@@ -6,33 +6,43 @@ const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
+const length =  lengthEl.value
+
 
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
-    number:getRandomNumber,
+    number: getRandomNumber,
     symbol: getRandomSymbol
 }
 
+clipboardEl.addEventListener('click', () => {
+    const password = resultEl.innerText;
+    if (!password) {
+        return;
+    }
+    navigator.clipboard.writeText(password);
+    alert('Password copied to clipboard!')
+})
+
 generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value 
     const hasLower = lowercaseEl.checked
     const hasUpper = uppercaseEl.checked
     const hasNumber = numbersEl.checked
     const hasSymbol = symbolsEl.checked
-
+    
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
 
-function generatePassword(lower,upper,number, symbol,length) {
-    let generatedPassword =''
-    const typesCount = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
     const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
     
     if(typesCount === 0) {
         return ''
     }
-
+    
     for(let i = 0; i < length; i += typesCount) {
         typesArr.forEach(type => {
             const funcName = Object.keys(type)[0]
@@ -40,21 +50,24 @@ function generatePassword(lower,upper,number, symbol,length) {
         })
     }
 
-    const finalPassword = generatedPassword.slice(0,length)
+    const finalPassword = generatedPassword.slice(0, length)
 
     return finalPassword
 }
 
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random()* 26)+97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
+
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random()* 26)+65)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
+
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random()* 10)+48)
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
+
 function getRandomSymbol() {
-    const symbols = '!@£$%^&*(){}[]=-+/,.'
-    return symbols[Math.floor(Math.random()*symbols.length)]
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
 }
