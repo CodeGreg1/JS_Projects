3d67b4a981bc6a40985e5f48580e87af52b56c08
GregWebDev <code@greggoodrem.co.uk>
2023-01-15 09:21:11 +0000

    Name edit 37

--- projects/37/script.js
+++ projects/37/script.js
@@ -37,6 +37,7 @@ const createPokemonCard = (pokemon) => {
 	pokemonEl.classList.add('pokemon')
 
 	const id = pokemon.id.toString().padStart(3, '0')
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
 
 	const poke_types = pokemon.types.map(type => type.type.name)
 	const type = main_types.find(type => poke_types.indexOf
